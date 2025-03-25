13910c42c7c70c4c9640514aeb8b858753a08775
Woochang Lee <changi112242@gmail.com>
2025-03-25 13:14:31 +0900

    스프링 고급: 글 삭제 처리

--- spring-framework-advanced/src/main/java/springframeworkadvanced/web/board/controller/BoardController.java
+++ spring-framework-advanced/src/main/java/springframeworkadvanced/web/board/controller/BoardController.java
@@ -2,12 +2,11 @@ package springframeworkadvanced.web.board.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import springframeworkadvanced.domain.board.Board;
 import springframeworkadvanced.web.board.dto.BoardRequestDto;
@@ -76,6 +75,13 @@ public class BoardController {
 
         return "board/boardView";
     }
+
+    @DeleteMapping("/board/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteBoard(@PathVariable Long id) {
+        boardService.delete(id);
+    }
+
 
     @GetMapping("/board/new")
     public String boardForm(Model model) {
