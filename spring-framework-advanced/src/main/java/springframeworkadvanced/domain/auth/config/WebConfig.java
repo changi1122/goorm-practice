e592fb0d7e726e6b60cabe67f964eec01cb40b53
Woochang Lee <changi112242@gmail.com>
2025-03-28 13:15:53 +0900

    스프링 고급: 인터셉터 추가

--- spring-framework-advanced/src/main/java/springframeworkadvanced/domain/auth/config/WebConfig.java
+++ spring-framework-advanced/src/main/java/springframeworkadvanced/domain/auth/config/WebConfig.java
@@ -4,10 +4,12 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springframeworkadvanced.domain.auth.filter.HeaderFilter;
 import springframeworkadvanced.domain.auth.interceptor.JwtTokenInterceptor;
+import springframeworkadvanced.domain.auth.interceptor.ReadPostLogInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -40,5 +42,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public JwtTokenInterceptor jwtTokenInterceptor() {
         return new JwtTokenInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new ReadPostLogInterceptor())
+                .addPathPatterns("/board/*");
     }
 }
