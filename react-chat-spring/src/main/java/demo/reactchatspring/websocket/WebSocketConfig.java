8b382e8ed67fcc4e532da80898b7bf4ec48facaf
Woochang Lee <changi112242@gmail.com>
2025-04-25 17:02:06 +0900

    #1 Spring: 웹푸시 의존성 추가 및 설정

--- react-chat-spring/src/main/java/demo/reactchatspring/websocket/WebSocketConfig.java
+++ react-chat-spring/src/main/java/demo/reactchatspring/websocket/WebSocketConfig.java
@@ -3,7 +3,6 @@ package demo.reactchatspring.websocket;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
